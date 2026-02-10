9beb451a2b264757922cff4a9f7fafe79d8d66af
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2026-02-10 17:26:38 +0900

    Tailwind css - 다크모드

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,90 @@
-import Image from "next/image";
+// src/app/page.tsx
+
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  // 1. 테마 상태 관리
+  const [theme, setTheme] = useState<"light" | "dark">(() => {
+    // 서버 환경이면 바로 light 반환
+    if (typeof window === "undefined") {
+      return "light";
+    }
+
+    // 로컬스토리지에 테마 값이 있다면 -> 해당 테마 반환
+    if (localStorage.getItem("theme")) {
+      return localStorage.getItem("theme") as "light" | "dark";
+    }
+
+    // 로컬스토리지에 테마 값이 없다면 -> 시스템 테마 따라감
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    }
+
+    // 기본값 light
+    return "light";
+  });
+
+  const toggleTheme = (newTheme: "light" | "dark" | "system") => {
+    // 시스템 테마를 선택한 경우 localStorage에 theme 삭제 & 시스템 테마 적용
+    if (newTheme === "system") {
+      localStorage.removeItem("theme");
+      setTheme(
+        window.matchMedia("(prefers-color-scheme: dark)").matches
+          ? "dark"
+          : "light",
+      );
+    } else {
+      // 로컬스토리지에 테마 값 저장 & 해당 테마 적용
+      localStorage.setItem("theme", newTheme);
+      setTheme(newTheme);
+    }
+  };
+
+  // theme 변경 시 루트 태그(html)에 dark 클래스 추가 -> 실제 다크모드 적용
+  useEffect(() => {
+    document.documentElement.classList.toggle("dark", theme === "dark");
+  }, [theme]);
+
+  // 시스템 테마 변경 감지
+  useEffect(() => {
+    const themeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const handleSystemThemeChange = (e: MediaQueryListEvent) => {
+      if (!localStorage.getItem("theme")) {
+        setTheme(e.matches ? "dark" : "light");
+      }
+    };
+
+    themeMediaQuery.addEventListener("change", handleSystemThemeChange);
+
+    return () => {
+      themeMediaQuery.removeEventListener("change", handleSystemThemeChange);
+    };
+  }, []);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div>
+      <div className="dark:bg-black dark:text-white">안녕</div>
+      <button
+        className="border bg-yellow-500 px-4 py-2"
+        onClick={() => toggleTheme("system")}
+      >
+        시스템
+      </button>
+      <button
+        className="border bg-white px-4 py-2"
+        onClick={() => toggleTheme("light")}
+      >
+        라이트
+      </button>
+      <button
+        className="border bg-black px-4 py-2 text-white"
+        onClick={() => toggleTheme("dark")}
+      >
+        다크
+      </button>
     </div>
   );
 }
