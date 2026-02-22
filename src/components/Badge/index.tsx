0d10c60f8a554e3983cd1c74c679c2d443663aa1
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2026-02-22 12:59:16 +0900

    storybook

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,32 +1,34 @@
 // src/components/Badge/index.tsx
 
-import { cva } from "class-variance-authority";
-import clsx from "clsx";
-import { twMerge } from "tailwind-merge";
-
-type BadgeProps = {
+interface BadgeProps {
+  size?: "xs" | "sm" | "md" | "lg";
+  colorPalette?: "gray" | "red" | "blue";
   children: React.ReactNode;
-  variant?: "secondary" | "destructive" | "outline";
-  className?: string;
-};
+}
 
-const badgeVariants = cva(
-  "px-2 py-1 text-xs font-medium bg-black text-white rounded-full",
-  {
-    variants: {
-      variant: {
-        secondary: "bg-gray-200 text-black",
-        destructive: "bg-red-500 text-white",
-        outline: "bg-transparent text-black border border-gray-200",
-      },
-    },
-  },
-);
+export default function Badge({
+  size = "md",
+  colorPalette = "gray",
+  children,
+}: BadgeProps) {
+  const sizeClass = {
+    xs: "text-xs px-2 py-1",
+    sm: "text-sm px-2.5 py-1.5",
+    md: "text-base px-3 py-2",
+    lg: "text-lg px-4 py-3",
+  };
 
-export default function Badge({ children, variant, className }: BadgeProps) {
-  return (
-    <span className={twMerge(clsx(badgeVariants({ variant, className })))}>
-      {children}
-    </span>
-  );
+  const colorPaletteClass = {
+    gray: "bg-gray-100 text-gray-800",
+    red: "bg-red-100 text-red-800",
+    blue: "bg-blue-100 text-blue-800",
+  };
+
+  const className = [
+    "inline-block rounded-full",
+    sizeClass[size],
+    colorPaletteClass[colorPalette],
+  ].join(" ");
+
+  return <div className={className}>{children}</div>;
 }
