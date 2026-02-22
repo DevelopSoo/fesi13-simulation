2f85b86c10dcc29ac2048d38bff8a86ce61c4ed3
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2026-02-22 13:07:10 +0900

    chromatic

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -14,7 +14,7 @@ export default function Badge({
   const sizeClass = {
     xs: "text-xs px-2 py-1",
     sm: "text-sm px-2.5 py-1.5",
-    md: "text-base px-3 py-2",
+    md: "text-base px-3 py-12",
     lg: "text-lg px-4 py-3",
   };
 
