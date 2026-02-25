cd491bd486d04bf09b17f60c6a1eb8714f15ba72
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2026-02-26 00:22:10 +0900

    husky 테스트

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -2,9 +2,9 @@
 
 import { render, screen } from "@testing-library/react";
 import Home from "./page";
-import { server } from "@/mocks/server";
-import { http, HttpResponse } from "msw";
 
-describe("MSW 모킹 테스트", () => {
-  test("MSW 상세 데이터 모킹", async () => {});
+test("메인 페이지 렌더링", () => {
+  render(<Home />);
+
+  expect(screen.getByText("Home")).toBeInTheDocument();
 });
