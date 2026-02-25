d610e503438c1f5f25075ce3a1fea490a9060bd4
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2026-02-26 00:49:28 +0900

    env-test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+// src/app/page.tsx
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">게시글 목록</h1>
+      <p>환경: {process.env.NEXT_PUBLIC_MY_ENVIRONMENT}</p>
+    </div>
+  );
 }
